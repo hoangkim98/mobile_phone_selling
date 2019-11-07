4e2138dbcee7deb0420084c3253abff163a1b549
HoangKim <hoangkim@HoangKims-MacBook-Pro.local>
2019-11-07 18:51:37 +0700

    change dbs config

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 class Database{
     // specify your own database credentials
     private $host = "localhost";
-    private $db_name = "assignment2";
+    private $db_name = "assignment";
     private $username = "root";
     private $password = "";
     public $conn;
