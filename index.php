25d6d979cd2befc289ad8ff60473ccc1c93b1cd0
tuanbieber <championship1604@gmail.com>
2019-11-08 08:55:35 +0700

    update home

--- index.php
+++ index.php
@@ -55,24 +55,24 @@
 					</div>
 				</div>
 
-				<div class="item-slick1" style="background-image: url(images/slide-07.jpg);">
+				<div class="item-slick1" style="background-image: url(images/xiaomi1.jpg);">
 					<div class="container h-full">
 						<div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
 							<div class="layer-slick1 animated visible-false" data-appear="rotateInDownLeft" data-delay="0">
 								<span class="ltext-101 cl2 respon2">
-									Maecenas aliquam
+									Xiaomi
 								</span>
 							</div>
 								
 							<div class="layer-slick1 animated visible-false" data-appear="rotateInUpRight" data-delay="800">
 								<h2 class="ltext-201 cl2 p-t-19 p-b-43 respon1">
-									Aenean aliquam
+									Phong cách trẻ trung
 								</h2>
 							</div>
 								
 							<div class="layer-slick1 animated visible-false" data-appear="rotateIn" data-delay="1600">
 								<a href="products.php" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-									Shop Now
+									Mua ngay
 								</a>
 							</div>
 						</div>
